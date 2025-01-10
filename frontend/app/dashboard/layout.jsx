a9472f9156be3bd004b516bb8b15455f4f9156df
Diana Moretti <morettidn@gmail.com>
2025-01-10 08:53:11 +0100

    update landing and dashboard style page

--- frontend/app/dashboard/layout.jsx
+++ frontend/app/dashboard/layout.jsx
@@ -3,7 +3,7 @@ import Sidenav from "../_components/sidenav";
 
 export default function Dashboardlayout({ children }) {
   return (
-    <div className="flex h-screen flex-col md:flex-row md:overflow-hidden">
+    <div className="min-h-screen overflow-y-auto flex h-screen flex-col md:flex-row">
       <div className="w-full flex-none md:w-64">
         <Sidenav />
       </div>
